31070b5614a69d424563854a8da32d9d5a0757e0
James Gore <73591234+jgore077@users.noreply.github.com>
2024-03-10 18:01:11 -0400

    modified:   MBartModel.py

--- MBartModel.py
+++ MBartModel.py
@@ -1,6 +1,46 @@
-from TranslationModelInterface import TranslationModelInterface
+from TranslationModelInterface import TranslationModelInterface,LanguageNotSupported
+from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
 
 class MBartModel(TranslationModelInterface):
     def __init__(self):
-        pass
+        self.model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
+        self.tokenizer = MBart50TokenizerFast.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
+        with open('mbart_langs.txt','r') as mbart_langs:
+            self.supported_languages=mbart_langs.read().splitlines()
+            
+    def translate(self,text:str,iso639_1_from:str,iso639_1_to:str)->str:
+        # Shortening the MBART language codes to iso639 codes
+        abbreviated_lang_codes=[lang_code[:2] for lang_code in self.supported_languages]
         
+        
+        if len(iso639_1_from)>2:
+            raise ValueError("Source Language Code Cannot Be More Than 2 Characters")
+        
+        if iso639_1_from not in abbreviated_lang_codes:
+            raise LanguageNotSupported("Source Language Not Supported Or Language Code Does Not Exist")
+        
+        if len(iso639_1_to)>2:
+            raise ValueError("Translation Language Code Cannot Be More Than 2 Characters")
+        
+        if iso639_1_to not in abbreviated_lang_codes:
+            raise LanguageNotSupported("Translation Language Not Supported Or Language Code Does Not Exist")
+        
+        # Getting MBART code from iso639 code
+        self.tokenizer.src_lang=self.supported_languages[abbreviated_lang_codes.index(iso639_1_from)]
+        
+        
+        encoded_ar = self.tokenizer(text, return_tensors="pt")
+        
+        generated_tokens = self.model.generate(
+            **encoded_ar,
+            forced_bos_token_id=self.tokenizer.lang_code_to_id[self.supported_languages[abbreviated_lang_codes.index(iso639_1_to)]]
+        )
+        
+        return self.tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+        
+
+    
+if __name__=="__main__":
+    mbart = MBartModel()
+    result=mbart.translate("Im testing the language model!","en","id")
+    print(result)
--- TranslationModelInterface.py
+++ TranslationModelInterface.py
@@ -3,3 +3,9 @@ class TranslationModelInterface:
     def translate(self,text)->str:
         # Any model class implemented with this
         raise NotImplementedError
+
+class LanguageNotSupported(Exception):
+    
+     def __init__(self, Message):
+        self.Message = Message
+        return
