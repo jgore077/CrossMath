a27167c7681eeb52ddd591293bffb8c19fc1c62b
JosephPolletta <jodotpo@gmail.com>
2024-04-11 16:02:31 -0400

    Added new Persian string to Demo.py and added utf-8 encoding to it

--- Demo.py
+++ Demo.py
@@ -1,9 +1,11 @@
+# coding=utf-8
 from langdetect import detect
 from LaTeXMasker import LaTeXMasker
 from TranslationModel import MBartModel
 
 mbart = MBartModel()
 masker = LaTeXMasker()
+
 
 # We have languages that are not supported that are regional dialects or descend from supported languages, so we covert
 # them to the parent language to attempt to translate.
@@ -29,14 +31,15 @@ beginningStrings = [
 "论欧拉马斯切罗尼常数的非理性",
 "Sur l'irrationalité de la constante d'Euler Mascheroni",
 "Ho il seguente problema: Sia $|x_{n+1} - x_n| < 1/3^n$. Mostra che $(x_n)$ è una successione di Cauchy.",
-"Ich habe folgendes Problem: Sei $|x_{n+1} - x_n| < 1/3^n$. Zeige, dass $(x_n)$ eine Cauchy-Folge ist."
+"Ich habe folgendes Problem: Sei $|x_{n+1} - x_n| < 1/3^n$. Zeige, dass $(x_n)$ eine Cauchy-Folge ist.",
+"می‌خواهم مقدار عبارت زیر را پیدا کنم: $$ 5^{133} \mod 8. $$. متوجه شدم که   هنگامی که $n$ فرد است و در غیر اینصورت برابر 1 است، که من را به این نتیجه می‌رساند که $5^{133} \mod 8 = 5$ اما نمی‌دانم چگونه این را اثبات کنم. چگونه می‌توانم اثبات کنم که این اتفاق می‌افتد (یا اگر این اتفاق نمی‌افتد، راه‌حل دیگری را پیدا کنم)؟"
 ]
 
 # Iterate over our test strings
 for sentence in beginningStrings:
 
     # Mask out the LaTeX equations
-    maskedSentence, unmaskDictionary = masker.Mask(sentence)
+    maskedSentence, unmaskDictionary = masker.mask(sentence)
 
     # Show the initial sentence and its masked version, will be removed in production.
     print("initial:")
@@ -52,7 +55,7 @@ for sentence in beginningStrings:
 
     # Translate the masked sentence to English and unmask
     maskedTranslatedSentence = mbart.translate(maskedSentence, language, "en")
-    translatedSentence = masker.Unmask(maskedTranslatedSentence[0], unmaskDictionary)
+    translatedSentence = masker.unmask(maskedTranslatedSentence[0], unmaskDictionary)
 
     # This is a current issue we are working on diagnosing, as when certain strings are masked, the translation instead
     # outputs this phrase, but translates correctly when translating the unmasked version, so until the issue is solved
--- LaTeXMasker/LaTeXMasker.py
+++ LaTeXMasker/LaTeXMasker.py
@@ -1,13 +1,14 @@
 from .ReplaceLaTeXEquations import LaTeXReplacer
 from .ReplaceDelimiterStrings import DelimterReplacer
 
+
 class LaTeXMasker():
     def __init__(self) -> None:
         self.latex = LaTeXReplacer()
         self.replacer = DelimterReplacer()
-    
-    def Mask(self,incomingString : str):
+
+    def mask(self, incomingString: str):
         return self.latex.ReplaceEquations(incomingString)
-    
-    def Unmask(self,incomingString : str, EquationSet : dict):
-        return self.replacer.ReplaceDelimiters(incomingString,EquationSet)
+
+    def unmask(self, incomingString: str, EquationSet: dict):
+        return self.replacer.ReplaceDelimiters(incomingString, EquationSet)
