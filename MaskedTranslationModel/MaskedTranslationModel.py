0f1af2a315249b255bceb7e947399277b2415c9d
James Gore <73591234+jgore077@users.noreply.github.com>
2024-04-12 00:22:55 -0400

    add temp edge case in MaskedTranslationModel

--- MaskedTranslationModel/MaskedTranslationModel.py
+++ MaskedTranslationModel/MaskedTranslationModel.py
@@ -15,6 +15,8 @@ class MaskedTranslationModel:
         match = re.match(r'[XYZ¿\?\.,!0-9 ]*', maskedString)
         if match.group() == "" or match.group() == "¿":
             translatedString=self.translater.translate(maskedString,iso639_1_from,iso639_1_to)
+            if translatedString[:83] == "The Committee recommends that the State party take all necessary measures to ensure":
+                translatedString = self.translater.translate(text, iso639_1_from, iso639_1_to)
             return self.masker.unmask(translatedString,maskedDict)
         return text
         
