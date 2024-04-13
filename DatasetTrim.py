3dafacc4a780d0f6b099f74ec9cbe21ed7c5a92f
JosephPolletta <jodotpo@gmail.com>
2024-04-12 21:48:05 -0400

    Updated test datasets and trimmed datasets using new mask and updated DatasetTrim.py to automatically place trimmed datasets in the correct folder.

--- DatasetTrim.py
+++ DatasetTrim.py
@@ -6,7 +6,7 @@ for code in codes:
         rd = csv.reader(fd, delimiter="\t", quotechar='"')
         fd.seek(0)
         rows = [row for row in rd]
-        with open(f"datasets/{code}Trimmed.tsv", 'w', encoding='utf-8') as translated_set:
+        with open(f"datasetsTrimmed/{code}Trimmed.tsv", 'w', encoding='utf-8') as translated_set:
             for row in rows:
                 row[2] = row[2][:2000]
                 for i in range(1, len(row)):
